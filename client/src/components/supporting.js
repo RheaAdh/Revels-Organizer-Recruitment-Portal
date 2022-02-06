e9fd382c6e0173898e92bf7cbb941931004e075b
Parthiv-M <parthiv.menon@learner.manipal.edu>
2022-02-06 16:17:02 +0530

    minor text edits

--- client/src/components/supporting.js
+++ client/src/components/supporting.js
@@ -159,7 +159,7 @@ const supporting = [
 ];
 const sys = {
   id: "sys",
-  title: "Web and System Admin",
+  title: "System Admin and Web Dev",
   description:
     "As we welcome various participants not only from our own college but also from different colleges to be a part of Revels, it’ll be your job to help maintain the database for each participant throughout as a part of the system admin team while also assisting with the the front-end and back-end development of the different payment procedures and Gateways. You need to create and maintain portals to ensure the smooth working of other committees.",
   video:
