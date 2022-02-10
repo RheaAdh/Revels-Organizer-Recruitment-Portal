cf0b7e025f62705ab45c8c62b456dca5334cca38
akash-4 <akashagarwal41200@gmail.com>
2022-02-10 11:34:39 +0530

    Changes

---
+++ server/models/cultural.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const pref = new mongoose.Schema({
+  category: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: Number,
+    default: 0,
+    enum: [0, 1, 2, 3],
+  },
+});
+
+const culturalSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  registration_no: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  branch: {
+    type: String,
+    required: true,
+  },
+  cgpa: {
+    type: Number,
+    require: true,
+  },
+  pref_1: pref,
+  pref_2: pref,
+  experience: {
+    type: String,
+  },
+  isEdited: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = {
+  Cultural: mongoose.model("Cultural", culturalSchema),
+};
