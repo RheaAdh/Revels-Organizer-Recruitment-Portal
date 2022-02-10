d785b2458952b5df662258a2124c1f31b97ac76c
akash-4 <akashagarwal41200@gmail.com>
2022-02-10 16:06:12 +0530

    Email Changed

--- server/routes/admin/auth.js
+++ server/routes/admin/auth.js
@@ -1,135 +1,221 @@
 const { Category, Token } = require("../../models/category");
 const bcrypt = require("bcryptjs");
 const jwtUtils = require("../../utils/jwt");
+const cultural = [
+  {
+    id: "Anubhuti",
+    title: "Anubhuti",
+    email: "revelsgoonj22@gmail.com",
+  },
+  {
+    // Infodesk and Finance
+    id: "Ampersand",
+    title: "Ampersand",
+    email: "revels.ampersand@gmail.com",
+  },
+  {
+    // App Dev
+    id: "Ergo",
+    title: "Ergo",
+    email: "ergo.revels@gmail.com",
+  },
+  {
+    id: "Psychus",
+    title: "Psychus",
+    email: "psychus.revelsmit@gmail.com",
+  },
+  {
+    id: "Kalakriti",
+    title: "Kalakriti",
+    email: "revelskalakriti2022@gmail.com",
+  },
+  {
+    id: "Crescendo",
+    title: "Crescendo",
+    email: "crescendo.revelscc@gmail.com",
+  },
+  {
+    id: "Footloose",
+    title: "Footloose",
+    email: "footloose.revels@gmail.com",
+  },
+  {
+    id: "HauteCouture",
+    title: "Haute Couture",
+    email: "hautecouturerevels22@gmail.com",
+  },
+  {
+    id: "Animania",
+    title: "Animania",
+    email: "revels.animania.mit@gmail.com",
+  },
+  {
+    id: "Xventure",
+    title: "Xventure",
+    email: "revels.xventure@gmail.com",
+  },
+  {
+    id: "HumanLibrary",
+    title: "Human Library",
+    email: "humanlibrary.revelsmit@gmail.com",
+  },
 
+  {
+    id: "Lensation",
+    title: "Lensation",
+    email: "lensation.revels22@gmail.com",
+  },
+  {
+    id: "Iridescent",
+    title: "Iridescent",
+    email: "iridecent.revels22@gmail.com",
+  },
+  {
+    id: "Altaebir",
+    title: "Altaebir",
+    email: "revelsfilmfestival2022@gmail.com",
+  },
+
+  {
+    id: "Consulere",
+    title: "Consulere",
+
+    email: "manipalacademy@180dc.org",
+  },
+  {
+    id: "Dramebaaz",
+    title: "Dramebaaz",
+    email: "dramebaaz.mit@gmail.com",
+  },
+];
+const categories = [
+  {
+    id: "superAdmin",
+    category: "SUPERADMIN",
+    email: "",
+  },
+  {
+    id: "sys",
+    category: "Web and System Admin",
+    email: "webdevrevels22@gmail.com",
+  },
+  {
+    id: "workshops",
+    category: "Workshops",
+    email: "workshops.revels22@gmail.com",
+  },
+  {
+    // Infodesk and Finance
+    id: "inf",
+    category: "Infodesk and Finance",
+    email: "inf.revels2022@gmail.com",
+  },
+  {
+    // App Dev
+    id: "app",
+    category: "App Dev",
+    email: "appdev.revels22@gmail.com",
+  },
+  {
+    id: "proshow",
+    category: "ProShow",
+    email: "proshow.revels22@gmail.com",
+  },
+  {
+    id: "certificate",
+    category: "Certificates and Prizes",
+    email: "logisticsrevels2022@gmail.com",
+  },
+  {
+    id: "logistics",
+    category: "Logistics",
+    email: "cnp.revels22@gmail.com",
+  },
+  {
+    id: "vigilance",
+    category: "Vigilance",
+    email: "vigilance.revels22@gmail.com",
+  },
+  {
+    id: "photography",
+    category: "Photography and Videography",
+    email: "pav.revels22@gmail.com ",
+  },
+  {
+    id: "informals",
+    category: "Informals",
+    email: "informals.revels22@gmail.com",
+  },
+  {
+    id: "hfs",
+    category: "HFS",
+    email: "hfs.revels22@gmail.com",
+  },
+  {
+    id: "judges",
+    category: "Judges",
+    email: "judgesrevels22@gmail.com",
+  },
+
+  {
+    id: "pnp",
+    category: "Publicity and Printing",
+    email: "pnprevels22@gmail.com",
+  },
+  {
+    id: "gaming",
+    category: "Gaming",
+    email: "gaming.revels22@gmail.com",
+  },
+  {
+    id: "outstation",
+    category: "Outstation Management",
+    email: "omrevels2022@gmail.com",
+  },
+
+  {
+    id: "socialmedia",
+    category: "Social Media",
+    email: "smcc.revels22@gmail.com",
+  },
+  {
+    id: "operations",
+    category: "Operations",
+    email: "operations.revels22@gmail.com",
+  },
+  {
+    id: "hrd",
+    category: "HRD",
+    email: "revels.hrd22@gmail.com",
+  },
+
+  {
+    id: "graphics",
+    category: "Graphics",
+    email: "graphics.revels22@gmail.com",
+  },
+  {
+    id: "sponsorhips",
+    category: "Sponsorships",
+    email: "sponsorship.revels22@gmail.com",
+  },
+
+  {
+    id: "sports",
+    category: "Sports",
+    email: "sports.revels22@gmail.com",
+  },
+];
 const register = async (req, res) => {
   try {
-    const categories = [
-      {
-        id: "superAdmin",
-        category: "SUPERADMIN",
-        email: "",
-      },
-      {
-        id: "sys",
-        category: "Web and System Admin",
-        email: "webdevrevels22@gmail.com",
-      },
-      {
-        id: "workshops",
-        category: "Workshops",
-        email: "workshops.revels22@gmail.com",
-      },
-      {
-        // Infodesk and Finance
-        id: "inf",
-        category: "Infodesk and Finance",
-        email: "inf.revels2022@gmail.com",
-      },
-      {
-        // App Dev
-        id: "app",
-        category: "App Dev",
-        email: "appdev.revels22@gmail.com",
-      },
-      {
-        id: "proshow",
-        category: "ProShow",
-        email: "proshow.revels22@gmail.com",
-      },
-      {
-        id: "certificate",
-        category: "Certificates and Prizes",
-        email: "logisticsrevels2022@gmail.com",
-      },
-      {
-        id: "logistics",
-        category: "Logistics",
-        email: "cnp.revels22@gmail.com",
-      },
-      {
-        id: "vigilance",
-        category: "Vigilance",
-        email: "vigilance.revels22@gmail.com",
-      },
-      {
-        id: "photography",
-        category: "Photography and Videography",
-        email: "pav.revels22@gmail.com ",
-      },
-      {
-        id: "informals",
-        category: "Informals",
-        email: "informals.revels22@gmail.com",
-      },
-      {
-        id: "hfs",
-        category: "HFS",
-        email: "hfs.revels22@gmail.com",
-      },
-      {
-        id: "judges",
-        category: "Judges",
-        email: "judgesrevels22@gmail.com",
-      },
-
-      {
-        id: "pnp",
-        category: "Publicity and Printing",
-        email: "pnprevels22@gmail.com",
-      },
-      {
-        id: "gaming",
-        category: "Gaming",
-        email: "gaming.revels22@gmail.com",
-      },
-      {
-        id: "outstation",
-        category: "Outstation Management",
-        email: "omrevels2022@gmail.com",
-      },
-
-      {
-        id: "socialmedia",
-        category: "Social Media",
-        email: "smcc.revels22@gmail.com",
-      },
-      {
-        id: "operations",
-        category: "Operations",
-        email: "operations.revels22@gmail.com",
-      },
-      {
-        id: "hrd",
-        category: "HRD",
-        email: "revels.hrd22@gmail.com",
-      },
-
-      {
-        id: "graphics",
-        category: "Graphics",
-        email: "graphics.revels22@gmail.com",
-      },
-      {
-        id: "sponsorhips",
-        category: "Sponsorships",
-        email: "sponsorship.revels22@gmail.com",
-      },
-
-      {
-        id: "sports",
-        category: "Sports",
-        email: "sports.revels22@gmail.com",
-      },
-    ];
-    categories.forEach(async (cat) => {
+    cultural.forEach(async (cat) => {
       const result = Math.random().toString(36).substring(2, 7);
       const pass = cat.id + "_" + result;
       // const salt = await bcrypt.genSalt(10);
       // const hash = await bcrypt.hash(pass, salt);
       const newCategory = new Category({
         categoryId: cat.id,
-        category: cat.category,
+        category: cat.title,
         email: cat.email,
         password: pass,
         token: null,
--- server/routes/admin/cred.js
+++ server/routes/admin/cred.js
@@ -1,11 +1,102 @@
 const { Category } = require("../../models/category");
 const main = require("../../utils/confirmationEmail");
 const credTemplate = require("../../utils/creds");
+const cultural = [
+  {
+    id: "Anubhuti",
+    title: "Anubhuti",
+    email: "revelsgoonj22@gmail.com",
+  },
+  {
+    // Infodesk and Finance
+    id: "Ampersand",
+    title: "Ampersand",
+    email: "revels.ampersand@gmail.com",
+  },
+  {
+    // App Dev
+    id: "Ergo",
+    title: "Ergo",
+    email: "ergo.revels@gmail.com",
+  },
+  {
+    id: "Psychus",
+    title: "Psychus",
+    email: "psychus.revelsmit@gmail.com",
+  },
+  {
+    id: "Kalakriti",
+    title: "Kalakriti",
+    email: "revelskalakriti2022@gmail.com",
+  },
+  {
+    id: "Crescendo",
+    title: "Crescendo",
+    email: "crescendo.revelscc@gmail.com",
+  },
+  {
+    id: "Footloose",
+    title: "Footloose",
+    email: "footloose.revels@gmail.com",
+  },
+  {
+    id: "HauteCouture",
+    title: "Haute Couture",
+    email: "hautecouturerevels22@gmail.com",
+  },
+  {
+    id: "Animania",
+    title: "Animania",
+    email: "revels.animania.mit@gmail.com",
+  },
+  {
+    id: "Xventure",
+    title: "Xventure",
+    email: "revels.xventure@gmail.com",
+  },
+  {
+    id: "HumanLibrary",
+    title: "Human Library",
+    email: "humanlibrary.revelsmit@gmail.com",
+  },
+
+  {
+    id: "Lensation",
+    title: "Lensation",
+    email: "lensation.revels22@gmail.com",
+  },
+  {
+    id: "Iridescent",
+    title: "Iridescent",
+    email: "iridecent.revels22@gmail.com",
+  },
+  {
+    id: "Altaebir",
+    title: "Altaebir",
+    email: "revelsfilmfestival2022@gmail.com",
+  },
+
+  {
+    id: "Consulere",
+    title: "Consulere",
+
+    email: "manipalacademy@180dc.org",
+  },
+  {
+    id: "Dramebaaz",
+    title: "Dramebaaz",
+    email: "dramebaaz.mit@gmail.com",
+  },
+];
 
 const creds = async (req, res) => {
   try {
+    let emails = [];
+    cultural.forEach((cat) => {
+      emails.push(cat.email);
+    });
     const categories = await Category.find(
-      {},
+      { email: emails },
       {
         category: 1,
         email: 1,
@@ -13,7 +104,8 @@ const creds = async (req, res) => {
         categoryId: 1,
       }
     );
-    for (var i = 20; i < 22; i++) {
+    // console.log(categories.length);
+    for (var i = 0; i < 10; i++) {
       cat = categories[i];
       console.log(cat.email);
       const message = credTemplate(cat.category, cat.categoryId, cat.password);
